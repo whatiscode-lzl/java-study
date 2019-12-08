fb30f65325501c7e131dd529e9713a2ee512325e
lamymay <lamymay@outlook.com>
2019-12-08 21:23:01 +0800

    Initial commit

---
+++ src/test/java/com/arc/nio/NioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arc.nio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
